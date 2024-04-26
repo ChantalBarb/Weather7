421d874340a4ce52e25c786b4dc470dafd090a55
ChantalBarb <159809008+ChantalBarb@users.noreply.github.com>
2024-04-26 17:47:57 +0200

    icon added

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ function refreshWeather(response){
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
-console.log(response.data);
+    console.log(response.data);
 
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
@@ -16,6 +17,7 @@ console.log(response.data);
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;    
     temperatureElement.innerHTML = Math.round(temperature);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 
@@ -37,8 +39,7 @@ function formatDate(date) {
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
-  
-        
+          
     return `${day} ${hours}:${minutes}`; 
 }
 
